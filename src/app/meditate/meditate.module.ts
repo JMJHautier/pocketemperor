13884c073f8649f83a39d6a7b100503dd54627b8
Jean <jhautier@hotmail.fr>
2022-05-31 14:54:53 +0200

    feat: add router

---
+++ src/app/meditate/meditate.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MeditateComponent } from './meditate.component';
+
+
+
+@NgModule({
+  declarations: [
+    MeditateComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MeditateModule { }
